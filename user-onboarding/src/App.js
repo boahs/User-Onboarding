812b4b77c2494d5badaca958ba170472ffc64584
boahs <39522778+boahs@users.noreply.github.com>
2020-08-19 18:54:26 -0400

    app.js almost fully built

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// import axios from "axios";
-// import * as yup from "yup";
+import axios from "axios";
+import * as yup from "yup";
 import Form from "./components/Form";
 
+const initialFormErrors = {
+  username: "",
+  email: "",
+  password: "",
+};
+const initialFormValues = {
+  ///// TEXT INPUTS /////
+  username: "",
+  email: "",
+  ///// DROPDOWN /////
+  password: "",
+  ///// RADIO BUTTONS /////
+};
+
+const initialUsers = [];
+const initialDisabled = true;
+
+const formSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Must be a valid email address")
+    .required("Must include email address"),
+  username: yup
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .required("Username is Required"),
+  password: yup
+    .string()
+    .min(8, "You must select a password of 8 or more characters")
+    .required("You must select a password"),
+});
+
 function App() {
+  const [users, setUsers] = useState(initialUsers); // array of friend objects
+  const [formValues, setFormValues] = useState(initialFormValues); // object
+  const [formErrors, setFormErrors] = useState(initialFormErrors); // object
+  const [disabled, setDisabled] = useState(initialDisabled); // boolean
+
+  const getUsers = () => {
+    axios
+      .get("https://reqres.in/api/users")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(`error: ${err}`);
+      });
+  };
+
+  const postNewUser = (newUser) => {
+    axios
+      .post("https://reqres.in/api/users", newUser)
+      .then((res) => {
+        setUsers([...users, res.data]);
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+      })
+      .finally(() => {
+        setFormValues(initialFormValues);
+      });
+  };
+
+  const inputChange = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,48 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as yup from "yup";
 
-const initialFormErrors = {
-  username: "",
-  email: "",
-  password: "",
-};
-const initialFormValues = {
-  ///// TEXT INPUTS /////
-  username: "",
-  email: "",
-  ///// DROPDOWN /////
-  password: "",
-  ///// RADIO BUTTONS /////
-};
-
 const initialUsers = [];
 const initialDisabled = true;
 
 function Form({ details }) {
-  const [users, setUsers] = useState(initialUsers); // array of friend objects
-  const [formValues, setFormValues] = useState(initialFormValues); // object
-  const [formErrors, setFormErrors] = useState(initialFormErrors); // object
-  const [disabled, setDisabled] = useState(initialDisabled); // boolean
-
-  const formSchema = (name, value) => {
-    yup
-      .reach(formSchema, name)
-      .validate(value)
-      .then((valid) => {
-        setFormErrors({
-          ...formErrors,
-          [name]: "",
-        });
-      });
-  };
-  console.log(initialFormErrors);
-
-  if (!details) {
-    return <h3>working on fetching your details...</h3>;
-  }
   return (
     <div className="form container">
-      <h2>{details.username}</h2>
+      <h2>hello</h2>
       <p></p>
       <p></p>
       <p></p>
