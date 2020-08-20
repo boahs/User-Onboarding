610f7f2364b539b8958939b7cc3fd4bccda7dce4
boahs <39522778+boahs@users.noreply.github.com>
2020-08-20 19:01:01 -0400

    name field test

--- user-onboarding/cypress/integration/form.spec.js
+++ user-onboarding/cypress/integration/form.spec.js
@@ -5,4 +5,12 @@ describe("User-Onboarding-App", () => {
       cy.url().should("include", "localhost");
     });
   });
+
+  describe("get the Name input and types a name into the field", () => {
+    it('can type something in the "text" input', () => {
+      cy.get('input[name="username"]')
+        .type("Testing")
+        .should("have.value", "Testing");
+    });
+  });
 });
