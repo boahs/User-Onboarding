b597776059cdfa92d8f9ef8b78fa32e8d660bd51
boahs <39522778+boahs@users.noreply.github.com>
2020-08-20 19:50:56 -0400

    testing submit button done

--- user-onboarding/cypress/integration/form.spec.js
+++ user-onboarding/cypress/integration/form.spec.js
@@ -11,8 +11,18 @@ describe("User-Onboarding-App", () => {
       cy.get('input[name="username"]')
         .type("Testing")
         .should("have.value", "Testing");
-      cy.get('input[name="password"]').type("hunter2");
+      cy.get('input[name="password"]').type("hunter2@");
       cy.get('input[name="email"]').type("email@email.com");
+    });
+  });
+  describe("Checkbox functionality", () => {
+    it("can check the terms of service box", () => {
+      cy.get('input[type="checkbox"]').first().check();
+    });
+  });
+  describe("submit button functionality", () => {
+    it("can submit a new user", () => {
+      cy.get("form").submit();
     });
   });
 });
